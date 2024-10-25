e4c06d2968ea8afdff9ac9faa3ff8408e4619dad
Prakash <olkaiyap2510@gmail.com>
2024-10-25 18:14:46 +0530

    1 test case orangeHRM

--- cypress/PageObject/Loginpage.js
+++ cypress/PageObject/Loginpage.js
@@ -1,7 +1,24 @@
-class Loginpage
-{
-    enterusername(username)
-    {
-    
+
+class LoginPage {
+    usernameInput = 'input[name="username"]';
+    passwordInput = 'input[name="password"]';
+    loginButton = 'button[type="submit"]';
+    alertMessage = '.oxd-alert-content-text';
+    visit() {
+        cy.visit('https://opensource-demo.orangehrmlive.com');
+    }
+    enterUsername(username) {
+        cy.get(this.usernameInput).type(username);
+    }
+    enterPassword(password) {
+        cy.get(this.passwordInput).type(password);
+    }
+    clickLogin() {
+        cy.get(this.loginButton).click();
+    }
+    getAlertMessage() {
+        return cy.get(this.alertMessage);
     }
 }
+
+export default new LoginPage();
--- cypress/e2e/Login.cy.js
+++ cypress/e2e/Login.cy.js
@@ -1,17 +1,19 @@
-
-
-describe('Login OrangeHRM', () => {
-
+import LoginPage from '../PageObject/Loginpage'
+describe('OrangeHRM Login Test', () => {
   beforeEach(() => {
-    cy.visit('https://opensource-demo.orangehrmlive.com');
-    cy.get('input[name="username"]').type('Admin');
-    cy.get('input[name="password"]').type('admin123');
-    cy.get('button[type="submit"]').click();
+    LoginPage.visit();
   });
-
-    it('Login in to website', () => 
-    {
-      cy.contains('PIM').click();
-    })
-
-  })
+  it('should log in successfully with valid credentials', () => {
+    LoginPage.enterUsername('Admin');
+    LoginPage.enterPassword('admin123');
+    LoginPage.clickLogin();
+    cy.url().should('include', '/dashboard');
+    cy.contains('Dashboard').should('be.visible');
+  });
+  it('should show an error message with invalid credentials', () => {
+    LoginPage.enterUsername('InvalidUser');
+    LoginPage.enterPassword('InvalidPassword');
+    LoginPage.clickLogin();
+    LoginPage.getAlertMessage().should('contain', 'Invalid credentials');
+  });
+});
