71dea139fd4d0d7d5b326befd906e6160eecd6dd
Prakash <olkaiyap2510@gmail.com>
2024-10-25 17:51:52 +0530

    leave module test cases

--- cypress/e2e/Login.cy.js
+++ cypress/e2e/Login.cy.js
@@ -2,13 +2,16 @@
 
 describe('Login OrangeHRM', () => {
 
-    
+  beforeEach(() => {
+    cy.visit('https://opensource-demo.orangehrmlive.com');
+    cy.get('input[name="username"]').type('Admin');
+    cy.get('input[name="password"]').type('admin123');
+    cy.get('button[type="submit"]').click();
+  });
 
     it('Login in to website', () => 
     {
-        cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-   
-      cy.get("input[placeholder='Username']").type("Admin")
+      cy.contains('PIM').click();
     })
 
   })
